da896f52f94f170213b0feacc501b15897d65a22
Mario Aburto <marioaburto10@gmail.com>
2017-09-15 16:34:07 -0400

    modal now pops up when clicking the Save button letting users know their venue was saved

--- app/components/Search/Results.js
+++ app/components/Search/Results.js
@@ -52,7 +52,27 @@ class Results extends React.Component {
 		                <em>{place.vicinity}     </em>
 		              </span>
 		              <span className="btn-group pull-right">
-		                <button className="btn btn-warning" onClick={() => this.handleClick(place)}>Save</button>
+		                <button type="button" className="btn btn-warning" data-toggle="modal" data-target="#myModal1">Save</button>
+		              	
+		              	<div id="myModal1" className="modal fade" role="dialog">
+						  <div className="modal-dialog">
+
+						    <div className="modal-content">
+						      <div className="modal-header">
+						        <button type="button" className="close" data-dismiss="modal">&times;</button>
+						        <h2 className="modal-title text-center">Venue Was Saved!</h2>
+						      </div>
+						      <div className="modal-body">
+						        <p>Please scroll up and click the Saved link to view your saved venues.</p>
+						      </div>
+						      <div className="modal-footer">
+						        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+						      </div>
+						    </div>
+
+						  </div>
+						</div>
+
 		              </span>
 		              <span className="btn-group pull-right">
 		                <button data-toggle="modal" data-target="#myModal" className="btn btn-success" onClick={() => this.viewHoursClick(place)} >View Hours</button>
